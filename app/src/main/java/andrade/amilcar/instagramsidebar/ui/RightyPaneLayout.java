2b1f789eb190257e4a0f799c529b0180a3c3d1ea
Amilcar Andrade <amilcar.andrade.g@gmail.com>
2018-09-21 14:47:57 -0700

    Clean up logic for setting the layout direction

--- app/src/main/java/andrade/amilcar/instagramsidebar/ui/RightyPaneLayout.java
+++ app/src/main/java/andrade/amilcar/instagramsidebar/ui/RightyPaneLayout.java
@@ -5,23 +5,23 @@ import android.support.v4.widget.SlidingPaneLayout;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class RightySlidingPaneLayout extends SlidingPaneLayout {
-    private final boolean isRtlDirection;
+public class RightyPaneLayout extends SlidingPaneLayout {
+    private final int deviceDirection;
 
-    public RightySlidingPaneLayout(Context context) {
+    public RightyPaneLayout(Context context) {
         this(context, null);
     }
 
-    public RightySlidingPaneLayout(Context context, AttributeSet attrs) {
+    public RightyPaneLayout(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public RightySlidingPaneLayout(Context context, AttributeSet attrs, int defStyle) {
+    public RightyPaneLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        // Ask the system if we are using a RTL
-        isRtlDirection = getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL;
+        // Ask the system for the layout direction
+        deviceDirection = getResources().getConfiguration().getLayoutDirection();
         // If the user is using RTL then use the default direction (left), otherwise use RTL as the default
-        setLayoutDirection(isRtlDirection ? View.LAYOUT_DIRECTION_LTR : View.LAYOUT_DIRECTION_RTL);
+        setLayoutDirection(deviceDirection == View.LAYOUT_DIRECTION_RTL ? View.LAYOUT_DIRECTION_LTR : View.LAYOUT_DIRECTION_RTL);
     }
 
     @Override
@@ -35,7 +35,7 @@ public class RightySlidingPaneLayout extends SlidingPaneLayout {
 
         for (int i = 0; i < childCount; i++) {
             // Apply the correct layout direction for the children
-            getChildAt(i).setLayoutDirection(isRtlDirection ? View.LAYOUT_DIRECTION_RTL : View.LAYOUT_DIRECTION_LTR);
+            getChildAt(i).setLayoutDirection(deviceDirection);
         }
     }
 }
